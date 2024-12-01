79632452a8be5542ddc2e4bb457b852bbc9a2eee
Furkan Doğan <doganfurkan440@gmail.com>
2024-12-01 04:36:20 +0300

    Add files via upload
    Fitness ile ilgilenmek isteyen veya ilgilenen kişiler için bir genel bilgi sistemi

---
+++ fitnessproje/Fitnessproje.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fitnessproje;
+
+/**
+ *
+ * @author TozLu
+ */
+public class Fitnessproje {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new giris().setVisible(true);
+            }
+        });
+    }
+    
+}
